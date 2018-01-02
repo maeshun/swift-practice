910f76721d0410f08b194faa9a455113a5a1e920
maeshun <butterflife2007@gmail.com>
2018-01-02 23:36:39 +0900

    電卓アプリ layout

---
+++ MyCalc/MyCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by 前俊輔 on 2018/01/02.
+//  Copyright © 2018年 Shunsuke Mae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var priceField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
