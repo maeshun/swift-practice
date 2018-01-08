0e5ce1fcc120b065fd8b0f88071bc3e121018eac
maeshun <butterflife2007@gmail.com>
2018-01-08 18:34:10 +0900

    ボタンタップ時の動作を実装

--- SampleQuiz/SampleQuiz/ViewController.swift
+++ SampleQuiz/SampleQuiz/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapSystemSound(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(1000) {
+            
+        }
+    }
+    
+    @IBAction func tapCustomSound(_ sender: Any) {
+        // サウンドファイルは再生時間が30秒以下であること
+        let soundUrl = Bundle.main.url(forResource: "custom", withExtension: "mp3")
+
+        // サウンドIDを登録する
+        var soundId: SystemSoundID = 0
+        // サウンドファイルを登録してサウンドIDを取得
+        AudioServicesCreateSystemSoundID(soundUrl! as CFURL, &soundId)
+        // 取得したサウンドIDを使用してサウンドを鳴らす
+        AudioServicesPlaySystemSoundWithCompletion(soundId) {
+            
+        }
+    }
+
+    @IBAction func tapVibration(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate) {
+            
+        }
+    }
+    
 
 }
 
